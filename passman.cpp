5654cef599aea1fc00a40f2bdbf32aa2957f0ffc
SauceyRed <42098474+SauceyRed@users.noreply.github.comd>
2022-10-14 15:47:31 +0200

    Added Credentials class and attempt at using SQLite3, there are errors I will fix later

--- passman.cpp
+++ passman.cpp
@@ -1,57 +1,139 @@
 #include <iostream>
 #include <fstream>
-#include "json.hpp" 
 #include <filesystem>
 #include <algorithm>
+#include <stdio.h>
+#include "sqlite3.h"
+
+class Credentials {
+    public:
+        std::string websiteURL;
+        std::string username;
+        std::string password;
+};
+
+void executeAction();
+void menu();
+void addCredentials();
+static int callback();
 
 int main()
 {
-    if (!std::filesystem::exists("database.json")) {
-        std::cout << "database.json does not exist, creating...\n";
-        std::ofstream { "database.json" };
-        std::cout << "database.json successfully created.\n";
-    }
+    menu();
+}
 
-    std::cout << "---------- Password Manager ----------\nOptions:\n1. Get credentials\n2. Add credentials\n3. Remove credentials\n";
-    int selectedOption;
-    std::cout << "> ";
-    std::cin >> selectedOption;
+void executeAction(std::string selectedOption)
+{
+    if (selectedOption == "1")
+    {
+        std::cout << "Option 1\n";
+    } else if (selectedOption == "2")
+    {
+        addCredentials();
+    } else if (selectedOption == "3")
+    {
+        std::cout << "Option 3\n";
+    } else
+    {
+        std::cout << "Invalid input";
+    }    
+    menu();
+}
 
-    switch(selectedOption) {
-        case 1:
-            // case 1
+void menu()
+{
+    std::string selectedOption;
+    while (true) {
+        std::cout << "---------- Password Manager ----------\nOptions:\n1. Get credentials\n2. Add credentials\n3. Remove credentials\nType \'exit\' to exit the program.\n";
+        std::cout << "> ";
+        std::cin >> selectedOption;
+        if (selectedOption == "1" || selectedOption == "2" || selectedOption == "3") {
+            std::cout << "selected option is good" << std::endl;
             break;
-        case 2:
-            std::string websiteURL;
-            std::string username;
-            std::string password;
-
-            bool randomPassGen = false;
-
-            std::cout << "Website URL: ";
-            std::cin >> websiteURL;
-            
-            std::cout << "Username/email: ";
-            std::cin >> username;
-
-            std::cout << "Password: ";
-            std::cin >> password;
-
-            /*
-            std::ifstream databaseFile("database.json");
-            nlohmann::json data = nlohmann::json::parse(databaseFile);
-            */
-           
-            /*
-            if (data.contains(websiteURL) && data.at(websiteURL) == username) {
-                std::string choice;
-                std::cout << "This username/email already exists for this website, do you wish to overwrite the credentials? (y/N) ";
-                std::cin >> choice;
-                std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
-                if (choice == "y" || choice == "yes") {
-                    data.at(websiteURL)
-                }
-            }
-            */
+        } else if (selectedOption == "exit")
+        {
+            exit(0);
+        }
+        
+        std::cout << "selected option is invalid" << std::endl;
     }
+    executeAction(selectedOption);
+}
+
+void addCredentials()
+{
+    Credentials newCredentials;
+
+    bool randomPassGen = false;
+
+    sqlite3 *db;
+    sqlite3_stmt * st;
+    int id = 0;
+    char *zErrMsg = 0;
+    int rc;
+
+    if (!std::filesystem::exists("credentials.db")) {
+        std::cout << "credentials.db does not exist, creating...\n";
+        
+        rc = sqlite3_open("credentials.db", &db);
+        
+        if (rc) {
+            fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+            menu();
+        } else {
+            fprintf(stderr, "Opened database successfully\n");
+        }
+
+        std::string sql = "CREATE TABLE credentials("
+                            "WEBSITE_URL TEXT NOT NULL, "
+                            "USERNAME TEXT NOT NULL, "
+                            "PASSWORD TEXT NOT NULL);";
+
+        sqlite3_close(db);
+
+        std::cout << "credentials.db successfully created.\n";
+    }
+
+    rc = sqlite3_open("credentials.db", &db);
+    
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        menu();
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    std::cout << "Website URL: ";
+    std::cin >> newCredentials.websiteURL;
+    
+    std::cout << "Username/email: ";
+    std::cin >> newCredentials.username;
+
+    std::cout << "Password: ";
+    std::cin >> newCredentials.password;
+
+    std::string sql = "INSERT INTO passwords (ID,title,password) VALUES (?,?,?)";
+
+    if(sqlite3_open("pw.db", &db) == SQLITE_OK)
+    {
+        sqlite3_prepare( db, sql.c_str(), -1 &st, NULL);
+        sqlite3_bind_text(st, 1, newCredentials.websiteURL.c_str(), newCredentials.websiteURL.length(), SQLITE_TRANSIENT);
+        sqlite3_bind_text(st, 2, newCredentials.username.c_str(), newCredentials.username.length(), SQLITE_TRANSIENT);
+        sqlite3_bind_text(st, 3, newCredentials.password.c_str(), newCredentials.password.length(), SQLITE_TRANSIENT);
+        sqlite3_step( st );
+    }  
+
+   std::cout << "Credentials added!\n";
+   std::cout << newCredentials.websiteURL << std::endl << newCredentials.username << std::endl << newCredentials.password << std::endl;
+
+   menu();
+}
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
 }
